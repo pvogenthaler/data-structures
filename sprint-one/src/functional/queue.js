0b6e8fc518c019cf8a50e7879df06737bdb5d939
pvogenthaler <paige.vogenthaler@gmail.com>
2016-11-02 15:31:44 -0700

    Implement queue and pass tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,31 @@
 var Queue = function() {
   var someInstance = {};
-
+  var size = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    size ++;
+    storage[size] = value;
   };
 
   someInstance.dequeue = function() {
+    if (size > 0) {
+      var temp = storage[1];
+      storage[1] = undefined;
+      for (var key in storage) {
+        storage[key - 1] = storage[key];      
+      }
+      storage[size] === undefined;
+      size --;
+      return temp;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
