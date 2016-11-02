86dd67cf375b4941b6db57465bd573c14e7f202a
pvogenthaler <paige.vogenthaler@gmail.com>
2016-11-02 15:00:32 -0700

    Implement stack and pass tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,24 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    size++;
+    storage[size] = value;
   };
 
   someInstance.pop = function() {
+    if (size > 0) {
+      var temp = storage[size];
+      storage[size] = undefined;
+      size--;
+      return temp;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
