523c141165914d06025cf62e8f952199252f7f47
pvogenthaler <paige.vogenthaler@gmail.com>
2016-11-02 17:46:14 -0700

    Implement functional-shared stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,13 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  var storage = {};
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  var storage = {};
+  someInstance.length = 0;
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
 var stackMethods = {};
+
+stackMethods.size = function() {
+  return this.length;
+};
+
+stackMethods.push = function(value) {
+  this.length++;
+  this[this.length] = value; 
+  return this.length;
+};
+
+stackMethods.pop = function() {
+  if (this.length > 0) {
+    var temp = this[this.length];
+    
+    this[length] = undefined;
+    this.length--;
+    console.log(this[this.length], this.length);
+    return temp;
+  }
+};
+
 
 
