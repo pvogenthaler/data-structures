9f9ea72f9eaa66aff17da24179b03dc11261d05b
pvogenthaler <paige.vogenthaler@gmail.com>
2016-11-03 14:17:28 -0700

    Implemented linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,32 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+
+    if (list.tail !== null) {
+      var previousTail = list.tail;
+      list.tail = Node(value);
+      previousTail.next = list.tail;
+    } else {
+      list.tail = Node(value);
+      list.head = list.tail;
+    }
   };
 
   list.removeHead = function() {
+    var previousHead = list.head;
+    list.head = list.head.next;
+    return previousHead.value;
   };
 
-  list.contains = function(target) {
+  list.contains = function(target, node) {
+    node = node || list.head;
+    if (node.value === target) {
+      return true;
+    } else if (node.next === null) {
+      return false;
+    } else {
+      return list.contains(target, node.next);
+    }
   };
 
   return list;
