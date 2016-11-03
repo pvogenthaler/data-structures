1d65ddb8591d89d9515bb4a3c873321a23d323ed
pvogenthaler <paige.vogenthaler@gmail.com>
2016-11-03 10:25:50 -0700

    Implement pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -4,9 +4,26 @@ var Queue = function() {
   this.length = 0;
 };
 
-Queue.prototype.enqueue = function() {};
+Queue.prototype.enqueue = function(value) {
+  this.length ++;
+  this[this.length] = value;
+  return this.length;
+};
 
-Queue.prototype.dequeue = function() {};
+Queue.prototype.dequeue = function() {
+  if (this.length > 0) {
+    var temp = this[1];
+    this[1] = undefined;
+    for (var key in this) {
+      this[key - 1] = this[key];
+    }
+    this[this.length] = undefined;
+    this.length --;
+    return temp;
+  }
+};
 
-Queue.prototype.size = function() {};
+Queue.prototype.size = function() {
+  return this.length;
+};
 
