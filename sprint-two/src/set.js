4a6b35b247c2e5e22b173f19e341f69bc985e65d
pvogenthaler <paige.vogenthaler@gmail.com>
2016-11-04 15:32:28 -0700

    Implement set and add time complexities

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.add = function(item) {     // O(1) - amortized constant
+  this._storage.push(item);
 };
 
-setPrototype.contains = function(item) {
+setPrototype.contains = function(item) {      // O(n) - linear
+  for (var i = 0; i < this._storage.length; i ++) {
+    if (this._storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
-
-setPrototype.remove = function(item) {
+ 
+setPrototype.remove = function(item) {      // O(n) - linear
+  for (var i = 0; i < this._storage.length; i ++) {
+    if (this._storage[i] === item) {
+      this._storage.splice(i, 1);
+    }
+  }
 };
 
 /*
