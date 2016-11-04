49ed036f5d9308762993ad1b04b10430607f0f09
pvogenthaler <paige.vogenthaler@gmail.com>
2016-11-03 19:45:38 -0700

    Began tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,28 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value)); 
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } else if (this.children.length > 0) {
+    for (var i = 0; i < this.children.length; i ++) {
+      console.log(this.children[i]);
+      return this.children[i].contains(target);
+    }
+  } else {
+    return false;
+  }
 };
 
 
