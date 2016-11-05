889a3f741fc455667633bb23c619417c1d5bf64f
pvogenthaler <paige.vogenthaler@gmail.com>
2016-11-04 22:08:22 -0700

    Start binary tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,22 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.right = null;
+  this.left = null;
 };
 
+BinarySearchTree.prototype.insert = function(val) {
+  if (this.value > val) {
+    // 
+  }
+};
 
+BinarySearchTree.prototype.contains = function() {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function() {
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
