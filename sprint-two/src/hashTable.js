55ad91ae6896472de2fb6934ece1072ab0674acc
pvogenthaler <paige.vogenthaler@gmail.com>
2016-11-05 14:28:42 -0700

    Start hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,9 +1,15 @@
 var HashTable = function() {    // O(1) - constant
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._count = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {     // O(n) - linear
+  
+  // if >75% storage full then run double
+  if ((this._count + 1) / this._limit > .75) {
+    this.reHash(this._limit * 2);
+  }
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
   var replaced = false;
@@ -24,7 +30,24 @@ HashTable.prototype.insert = function(k, v) {     // O(n) - linear
   }
 
   this._storage.set(index, bucket);
-  
+  this._count++;
+};
+
+HashTable.prototype.reHash = function(newLimit) {
+  var temp = [];
+  this._storage.each(function(bucket) {
+    if (bucket !== undefined) {
+      for (var i = 0; i < bucket.length; i++) {
+        temp.push(bucket[i]);
+        this._storage.set(i, undefined);
+      }
+    }
+  });
+  this._limit = newLimit;
+  for (var i = 0; i < temp.length; i++) {
+    this.insert(temp[i][0], temp[i][1]);
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k) {    // O(n) - linear
